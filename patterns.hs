3e1b86f054d353ddc8120c932dde628e7af4efcc
Adrian Tudor Panescu <panescu.adrian.noreply@gmail.com>
2012-02-05 12:21:14 +0100

    added some more patterns, finished Chapter 3

--- patterns.hs
+++ patterns.hs
@@ -39,6 +39,10 @@ head' :: [a] -> a
 head' [] = error "Can't call head on an empty list, dummy!"
 head' (x:_) = x
 
+head'' :: [a] -> a
+head'' xs = case xs of [] -> error "Can't call head on an empty list, dummy!"
+                       (x:xs) -> x
+
 tell :: (Show a) => [a] -> String
 tell [] = "The list is empty"
 tell (x:[]) = "The list has one element: " ++ show x
@@ -75,3 +79,64 @@ myCompare x y
     | x == y    = EQ
     | x <= y    = LT
     | otherwise = GT
+
+badGreeting :: String
+badGreeting = "Oh! Pfft. It's you."
+
+niceGreeting :: String
+niceGreeting = "Hello! So very nice to see you, "
+
+greet :: String -> String
+greet "Juan" = niceGreeting ++ " Juan!"
+greet "Fernando" = niceGreeting ++ " Fernando!"
+greet name = badGreeting ++ " " ++ name
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+initials' :: String -> String -> String
+initials' firstname lastname = show f ++ ". " ++ show l ++ "."
+    where f = head firstname
+          l = head lastname 
+
+initials'' :: String -> String -> String
+initials'' (f:firstname) (l:lastname) = [f] ++ ". " ++ [l] ++ "." 
+
+calcBmi :: [(Double, Double)] -> [Double]
+calcBmi xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
+
+calcBmi' :: [(Double, Double)] -> [Double]
+calcBmi' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi > 25.0]
+
+cylinder :: Double -> Double -> Double
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in sideArea + 2 * topArea
+
+cylinder' :: Double -> Double -> Double
+cylinder' r h = sideArea + 2 * topArea
+    where sideArea = 2 * pi * r * h
+          topArea = pi * r ^ 2
+
+describeList :: [a] -> String
+describeList ls = "The list is " ++ case ls of [] -> "empty."
+                                               [x] -> "a singleton list."
+                                               xs -> "a longer list."
+
+describeList' :: [a] -> String
+describeList' ls = "The list is " ++ what ls
+    where what [] = "empty."
+          what [x] = "a singleton list."
+          what xs = "a longer list."
+
+
+describeList'' :: [a] -> String
+describeList'' ls =
+    let what xs = case xs of [] -> "empty."
+                             [x] -> "a singleton list."
+                             xs -> "a longer list."
+    in "The list is " ++ what ls
