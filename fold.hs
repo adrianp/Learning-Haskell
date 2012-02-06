a1184fbbbbb7e98ff6c66a966a83e24e1526bad2
Adrian Tudor Panescu <panescu.adrian.noreply@gmail.com>
2012-02-06 13:19:32 +0100

    finished folds

--- fold.hs
+++ fold.hs
@@ -1,3 +1,6 @@
+-- foldr f z [1,2,3] = f 1 (f 2 ( f 3 z))
+-- foldl f [1,2,3] z = f ( f (f z 1) 2) 3
+
 sum' :: (Num a) => [a] -> a
 sum' xs = foldl (\ acc x -> acc + x) 0 xs
 
@@ -19,3 +22,23 @@ elem'' x xs = foldr (\ y acc -> if x == y then True else acc) False xs
 --max 1 2 = 2
 maximum'' :: (Ord a) => [a] -> a
 maximum'' = foldl1 max
+
+reverse'' :: [a] -> [a]
+-- we cannot do [] : x but we can x : []
+reverse'' = foldl (\ acc x -> x : acc) [] 
+
+reverse''' :: [a] -> [a]
+reverse''' = foldl (flip (:)) []
+
+
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
+
+filter'' :: (a -> Bool) -> [a] -> [a]
+filter'' p = foldr (\ x acc -> if p x then x : acc else acc) []
+
+last' :: [a] -> a
+last' = foldl1 (\ _ x -> x)
+
+and' :: [Bool] -> Bool
+and' xs = foldr (&&) True xs
